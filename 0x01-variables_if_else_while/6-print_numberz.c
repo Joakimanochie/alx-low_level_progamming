617e01bdbba2f59146259595da1f0e81c990384c
Joakimanochie <anochietobe@gmail.com>
2023-02-17 20:06:21 +0100

    0x01 C scripts

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10 ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
